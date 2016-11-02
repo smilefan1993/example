5d5202abf5e7c57baf9187ede4269fc03f1a8771
Dmitry Lubetsky <lubetskydy@yandex.com>
2016-11-02 15:39:03 +0300

    Migrations Added

--- app/DoctrineMigrations/Version20161102151942.php
+++ app/DoctrineMigrations/Version20161102151942.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Auto-generated Migration
  */
 class Version20161102151942 extends AbstractMigration
 {
