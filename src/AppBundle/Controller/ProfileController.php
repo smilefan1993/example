4ac2c3657acf62589afa8a380e6e155867f1eeae
Dmitry Lubetsky <lubetskydy@yandex.com>
2016-11-09 17:16:05 +0300

    HTML form for mail are added

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -2,7 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\User;
+
+
 class ProfileController extends Controller
 {
+    public function profileAction()
+    {
 
+    }
 }
